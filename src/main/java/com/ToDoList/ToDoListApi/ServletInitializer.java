7060575c94fac9cba4b3e893d7b9b6dc3e5e7ad7
Gerson Santos <Gerson Santos@GersonSantos-PC>
2017-07-15 09:39:56 -0600

    First Commit

---
+++ src/main/java/com/ToDoList/ToDoListApi/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.ToDoList.ToDoListApi;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ToDoListApiApplication.class);
+	}
+
+}
---
+++ src/main/java/com/ToDoList/ToDoListApi/ToDoListApiApplication.java
@@ -0,0 +1,68 @@
+package com.ToDoList.ToDoListApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Collection;
+import java.util.stream.Stream;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+public class ToDoListApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListApiApplication.class, args);
+	}
+}
+
+@Entity
+class Reservation {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String reservationName;
+    public Long getId() {
+        return id;
+    }
+    public String getReservationName() {
+        return reservationName;
+    }
+    public Reservation(String reservationName) {
+        this.reservationName = reservationName;
+    }
+    public Reservation() {
+    }
+    @Override
+    public String toString() {
+        return "Reservation{" + "id=" + id + ", reservationName=" + reservationName + '}';
+    }
+}
+
+@RepositoryRestResource //Esto es para usar Spring Data y se pretende usar Hibernate
+interface ReservationRepository extends JpaRepository {
+    @RestResource(path = "by-name")
+    Collection findByReservationName(@Param("rn") String rn);
+}
+
+@Component
+class DummyCLR implements CommandLineRunner {
+    @Autowired
+    private ReservationRepository repository;
+    @Override
+    public void run(String... args) throws Exception {
+        Stream.of("Josh", "Geertjan", "Pieter").forEach(x -> 
+           repository.save(new Reservation(x)));
+        repository.findAll().forEach(System.out::println);
+    }
+}
